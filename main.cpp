dcc582af6bef4c5e6d1b8ddfd573fc7e721e5a9b
MathZIVota <106335470+MathZIVota@users.noreply.github.com>
2023-05-15 18:12:19 +0000

    The input format is made as required: n,W - v1 w1 ... vn wn. As well as output via the sequence 0 and 1

--- main.cpp
+++ main.cpp
@@ -1,49 +1,73 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+/*
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int N; cout << "N:"; cin >> N;
+    vector<pair<pair<int,int>,int>> vec(N);
+    int i=1;
+    for(auto &v: vec) {v.first.first = i*i; v.first.second = -i*i; v.second = i; i++;}
+    for(auto &v: vec) cout << v.second << ":" << v.first.first << " " << v.first.second << endl;
+    return 0;
+}
+*/
 #include<vector>
 #include<iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-void Input(vector<pair <int, int>> &B,int N){
-  int v,w;
-  for(int i=0;i<N;i++){
-    v = i+1;
-    w = i*i+1;
-    B.push_back(pair<int, int>(v,w));
+void Input(vector<pair<pair<int, int>,int>> &B,int N){
+    int i=0;
+  for(auto &p:B){
+    cin >> p.first.first >> p.first.second;
+    p.second = i;
+    i++;
   }
+  
 }
-bool cmp(pair<int,int> a, pair<int,int> b){
-  return a.first/a.second < b.first/b.second;
+bool cmp(pair<pair<int,int>,int> a, pair<pair<int,int>,int> b){
+  return ((a.first.first)/(a.first.second) < (b.first.first)/(b.first.second));
 }
+
 int main()
 {
-  vector<pair <int, int>> Bag_Start; //first - value, second - weight
-  int n; cout << "n: "; cin >> n;
+  int n, W;  cin >> n >> W;
+  vector<pair<pair<int, int>,int>> Bag(n); //first - value, second - weight
+  cout << "Size Bag: " << Bag.size() << endl;
+  Input(Bag, n);
   
-  Input(Bag_Start, n);
-   cout << "------\n";
-  for(auto p: Bag_Start)  cout << p.first << " " << p.second << endl;
+  std::sort(Bag.begin(), Bag.end(),cmp);
   
-  std::sort(Bag_Start.begin(), Bag_Start.end(),cmp);
-   cout << "------\n";
-  for(auto p: Bag_Start)  cout << p.first << " " << p.second << endl;
-  cout << "------\n";
-  
-  vector<pair <int, int>> Bag;
-  int W; cout << "W: "; cin >> W;
+  cout << "n:" << n << endl;
+  cout << "W:" << W << endl;
+  cout << "-------\n";
+  for(auto p: Bag)  cout << p.second << ":"<< p.first.first << " " << p.first.second << endl;
+   cout << "-------\n";
+   
   int weight_tmp = W;
   int sum_value = 0;
+  vector<int> flag(n);
   
-  for(auto p: Bag_Start){
-    if(p.second < weight_tmp){
-      sum_value += p.first; 
-      weight_tmp -= p.second;
-      Bag.push_back(p);
-      cout << p.first << " " << p.second << endl;
-      cout<< sum_value << endl;
+  for(auto p: Bag){
+    if(p.first.second <= weight_tmp){
+      sum_value += p.first.first; 
+      weight_tmp -= p.first.second;
+      flag[p.second] = 1;
       
     } 
   }
+  for(auto x: flag) cout << x << " ";
+  cout << endl;
   cout << "------\nVALUE: " << sum_value << endl;
-  for(auto p: Bag)  cout << p.first << " " << p.second << endl;
-    return 0;
+  for(auto p: Bag)  cout << p.second << ":" << p.first.first << " " << p.first.second << endl;
+  return 0;
 }
